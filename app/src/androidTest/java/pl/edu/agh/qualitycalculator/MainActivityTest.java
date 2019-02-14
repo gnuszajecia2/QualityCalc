5007623b4d29a4693afd2f1692f4a0c0965fbf94
ChmielU <przemekchmielu@interia.pl>
2019-02-14 23:33:47 +0100

    Poprawione testy v5 (literowka)

--- app/src/androidTest/java/pl/edu/agh/qualitycalculator/MainActivityTest.java
+++ app/src/androidTest/java/pl/edu/agh/qualitycalculator/MainActivityTest.java
@@ -43,7 +43,7 @@ public class MainActivityTest {
         onView(withId(R.id.etNum1)).perform(click()).perform(typeText("4"));
         onView(withId(R.id.etNum2)).perform(click()).perform(typeText("4"));
         onView(withId(R.id.btnMult)).perform(click());
-        onView(withId(R.id.tvResult)).check(matches(withText("4.0 multiply 4.0 gives value 16.0")));
+        onView(withId(R.id.tvResult)).check(matches(withText("4.0 multiplied by 4.0 gives value 16.0")));
     }
 
     @Test
